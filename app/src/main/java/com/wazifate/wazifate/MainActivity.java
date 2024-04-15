5c1c44a6bca3f7fecae57662bc2791998670b8a7
BahaaAY <bahaa.abouyassine2@gmail.com>
2024-04-15 14:26:54 +0300

    Jobs ListView

--- app/src/main/java/com/wazifate/wazifate/MainActivity.java
+++ app/src/main/java/com/wazifate/wazifate/MainActivity.java
@@ -2,13 +2,37 @@ package com.wazifate.wazifate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.wazifate.wazifate.HomeScreen.HomeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button loginButton;
+    Context mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+        loginButton= findViewById(R.id.loginButton);
+        mContext = this;
+        loginButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(mContext, HomeActivity.class);
+                startActivity(i);
+
+            }
+        });
+
     }
+
+
+
+
 }
