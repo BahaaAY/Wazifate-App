cadcb9a4206112b1907db8e701bf34e85791d88e
BahaaAY <bahaa.abouyassine2@gmail.com>
2024-04-23 06:19:07 +0300

    Sign Up Screen Done!

--- app/src/main/java/com/wazifate/wazifate/MainActivity.java
+++ app/src/main/java/com/wazifate/wazifate/MainActivity.java
@@ -7,24 +7,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.wazifate.wazifate.HomeScreen.HomeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     Button loginButton;
+    TextView signUpButton;
     Context mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         loginButton= findViewById(R.id.loginButton);
+        signUpButton= findViewById(R.id.signupButton);
         mContext = this;
         loginButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(mContext, HomeActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(mContext, SignUp.class);
                 startActivity(i);
 
             }
